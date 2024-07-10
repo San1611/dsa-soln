13d86899055de44fe056864b69d897742c8fe703
San1611 <142013307+San1611@users.noreply.github.com>
2024-07-10 21:19:24 +0530

    Time: 1 ms (99.42%), Space: 42.7 MB (5.90%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        String str="";
+        int count=0;
+        for(int i=0;i<logs.length;i++){
+            if(logs[i].equals("../")){
+                if(count!=0)
+                count--;
+            }
+            else if(logs[i].equals("./")){
+                continue;
+            }
+            else{
+                count++;
+            }
+        
+        }        
+
+        return count;
+    }
+}
